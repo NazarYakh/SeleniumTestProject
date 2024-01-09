b3574af782dff7536fd70fe028a3af61c02b7353
Nazar Yakh <nazaryakh@gmail.com>
2024-01-09 16:59:12 +0100

    Added assertion message

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -9,10 +9,10 @@ class TestHomePage(BaseClass):
         shop_page = self.go_to_the_shop_page()
         shop_page_url = self.get_url()
 
-        assert ("shop" in shop_page_url)
+        assert "shop" in shop_page_url, "Url should contain shop-word"
 
         home_page = self.go_to_the_home_page()
         arrivals_amount = len(home_page.get_arrivals())
 
-        assert (arrivals_amount == 3)
+        assert arrivals_amount == 3, "Arrivals amount should be equal to 3"
 
